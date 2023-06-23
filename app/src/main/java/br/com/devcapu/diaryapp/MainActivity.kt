5b06380cb122f9db584c09f48d616d42940ba6b3
Felipe Moreno Borges <felipe.b2014@gmail.com>
2023-06-23 13:57:11 -0300

    :construction: Google Auth

--- app/src/main/java/br/com/devcapu/diaryapp/MainActivity.kt
+++ app/src/main/java/br/com/devcapu/diaryapp/MainActivity.kt
@@ -3,6 +3,9 @@ package br.com.devcapu.diaryapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import br.com.devcapu.diaryapp.navigation.Screen
+import br.com.devcapu.diaryapp.navigation.SetupNavGraph
 import br.com.devcapu.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +13,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiaryAppTheme {
-
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = Screen.Authentication.route,
+                    navController = navController
+                )
             }
         }
     }
---
+++ app/src/main/java/br/com/devcapu/diaryapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package br.com.devcapu.diaryapp.util
+
+object Constants {
+    const val APP_ID = "diaryapp-qimek"
+    const val CLIENT_ID = "108336446901-gb7d1pg3rt2mbru0e9p6k50kn4q490sm.apps.googleusercontent.com"
+
+    const val WRITE_SCREEN_ARGUMENT_KEY = "diaryId"
+}
